d8cb162c3f7038b5b487f7ead866151c9db847c1
Miguel Barrios Davila <miguelbarrios67@gmail.com>
2020-06-16 23:16:51 -0500

    final

---
+++ tickerSymbolScraper.py
@@ -0,0 +1,33 @@
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+import requests
+
+def my_function(exchange, outputFileName):
+	exchanges = {
+	"nyse":'https://www.advfn.com/nyse/newyorkstockexchange.asp?companies={letter}',
+	"nasdaq": "https://www.advfn.com/nasdaq/nasdaq.asp?companies={letter}"}
+
+	url = exchanges.get(exchange)
+	f = open(outputFileName, 'a')
+
+	symbols = "ABCDEFGHIJKLMNOPQUSTUVWXYZ+"
+	for c in symbols:
+		curUrl = url.format(letter = c)
+		data = requests.get(curUrl)
+		raw_html = data.text
+		soup = BeautifulSoup(raw_html, 'html.parser')
+		content = soup.find_all("table", {"class", "market tab1"})
+		rows = content[0].find_all("tr")
+		for i in range(2,len(rows)):
+			td = rows[i].find_all("td")
+			symbol = td[1].getText()
+			if len(symbol) != 0:
+				f.write(symbol)
+				f.write("\n")
+	f.close()
+
+my_function("nasdaq", "nasdaqSymbols.txt")
+
+
